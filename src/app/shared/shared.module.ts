031f61a7a75d2c1afbb8088d24baa8f116973ee6
lucasgabrielrm <lucasgmnzs@gmail.com>
2021-09-27 12:38:54 -0300

    Animações e Snackbar
    Instalação dos elementos para utilizar animação e criação de uma snackbar para informar quando itens foram adicionados ou removidos do carrinho de compras.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,22 +5,24 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 import {InputComponent} from './input/input.component'
 import {RadioComponent} from './radio/radio.component'
 import {RatingComponent} from './rating/rating.component'
+import {SnackbarComponent} from './messages/snackbar/snackbar.component'
 
 import {OrderService} from '../order/order.service'
 import {ShoppingCartService} from '../rest-detail/shopping-cart/shopping-cart.service'
 import {RestaurantsService} from '../restaurants/restaurants.service'
+import {NotificationService} from './messages/notification.service'
 
 @NgModule({
-  declarations: [InputComponent, RadioComponent, RatingComponent],
+  declarations: [InputComponent, RadioComponent, RatingComponent, SnackbarComponent],
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
-  exports: [InputComponent, RadioComponent, RatingComponent,
+  exports: [InputComponent, RadioComponent, RatingComponent, SnackbarComponent,
             CommonModule, FormsModule, ReactiveFormsModule]
 })
 export class SharedModule {
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: SharedModule,
-      providers:[ShoppingCartService, RestaurantsService, OrderService]
+      providers:[ShoppingCartService, RestaurantsService, OrderService, NotificationService]
     }
   }
 }
